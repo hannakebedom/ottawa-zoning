8c6a3dabe944b6d27e877f8f97daba11f7526242
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-03-26 15:32:51 -0400

    wip

--- get_zoning_data.py
+++ get_zoning_data.py
@@ -1,6 +1,22 @@
+# https://developers.arcgis.com/python/guide/working-with-feature-layers-and-features/
+
 from arcgis.features import FeatureLayer
 from IPython.display import display
 
-layer_url = "https://services8.arcgis.com/j9ezxktwsGMiHmNz/ArcGIS/rest/services/City_of_Ottawa_Zoning/FeatureServer/0"
-layer = FeatureLayer(layer_url)
-print(layer)
+feature_layer_url = "https://services8.arcgis.com/j9ezxktwsGMiHmNz/ArcGIS/rest/services/City_of_Ottawa_Zoning/FeatureServer/0"
+feature_layer = FeatureLayer(feature_layer_url)
+
+# list properties of the layer
+for f in feature_layer.properties.fields:
+    print(f['name'])
+
+# list the attributes of the first feature
+feature_set = feature_layer.query()
+features = feature_set.features
+
+# each feature consists of a set of rings (points) that define the polygon
+print(features[0].attributes)
+print(features[0].geometry)
+
+# NEXT: https://gis.stackexchange.com/questions/294206/how-to-create-a-simple-polygon-from-coordinates-in-geopandas-with-python
+
