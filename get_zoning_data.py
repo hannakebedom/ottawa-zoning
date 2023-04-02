c6ab4cf23c8f649511dda68fbe0d66b3dfedf65f
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-04-02 16:29:13 -0400

    finally got a polygon to show in the right spot

--- get_zoning_data.py
+++ get_zoning_data.py
@@ -4,18 +4,27 @@ import folium
 from arcgis.gis import GIS
 from arcgis.features import FeatureLayer
 from arcgis.geometry import Polygon
-import geopandas as gpd
-# from shapely.geometry import Polygon
-from IPython.display import display
-import numpy as np
-import webbrowser
+import pyproj
 
 feature_layer_url = "https://services8.arcgis.com/j9ezxktwsGMiHmNz/ArcGIS/rest/services/City_of_Ottawa_Zoning/FeatureServer/0"
 feature_layer = FeatureLayer(feature_layer_url)
 feature_set = feature_layer.query()
 features = feature_set.features
-
 geometry = features[0].geometry
+
+# define the projection systems
+in_proj = pyproj.Proj(init='epsg:3857')
+out_proj = pyproj.Proj(init='epsg:4326')
+
+# convert the coordinates to latitude and longitude
+lon_lat = []
+for ring in geometry['rings']:
+    lon_lat.append([pyproj.transform(in_proj, out_proj, x, y) for x, y in ring])
+
+
+
+
+print(geometry)
 polygon = Polygon(geometry)
 
 
