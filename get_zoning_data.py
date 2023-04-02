e5a420a964049eb624c6f48f8584aad9e7d4c780
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-04-02 17:29:46 -0400

    remove /usr/local/bin/python /Users/hannakebedom/github/ottawa-zoning/get_zoning_data.py

--- get_zoning_data.py
+++ get_zoning_data.py
@@ -6,15 +6,12 @@ from arcgis.features import FeatureLayer
 from arcgis.geometry import Polygon
 import pyproj
 
-feature_layer_url = "https://services8.arcgis.com/j9ezxktwsGMiHmNz/ArcGIS/rest/services/City_of_Ottawa_Zoning/FeatureServer/0"
-feature_layer = FeatureLayer(feature_layer_url)
-feature_set = feature_layer.query()
-features = feature_set.features
-geometry = features[0].geometry
-
-# define the projection systems
-in_proj = pyproj.Proj(init='epsg:3857')
-out_proj = pyproj.Proj(init='epsg:4326')
+def get_features(feature_layer_url = "https://services8.arcgis.com/j9ezxktwsGMiHmNz/ArcGIS/rest/services/City_of_Ottawa_Zoning/FeatureServer/0"):
+    '''retrieve features from the specified url'''
+    feature_layer = FeatureLayer(feature_layer_url)
+    feature_set = feature_layer.query()
+    features = feature_set.features
+    return features
 
 # convert the coordinates to latitude and longitude
 lon_lat = []
@@ -23,13 +20,9 @@ for ring in geometry['rings']:
 
 
 
-
+# main 
 print(geometry)
 polygon = Polygon(geometry)
 
 
-# TODO: Try using the ArcGIS map widget instead of Folium: 
-#   https://developers.arcgis.com/python/guide/part1-introduction-to-using-the-map-widget/
-#   https://developers.arcgis.com/python/guide/part2-working-with-geometries/
-# FOLIUM: https://gis.stackexchange.com/questions/294206/how-to-create-a-simple-polygon-from-coordinates-in-geopandas-with-python
 
