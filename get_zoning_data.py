3ddb744db3f8a66f3e57af6b3b7b6a5407b878e1
Hanna Kebedom <hanna.a.keb@gmail.com>
2023-04-10 21:52:16 -0400

    final verions of maps

--- get_zoning_data.py
+++ get_zoning_data.py
@@ -1,12 +1,14 @@
 # https://developers.arcgis.com/python/guide/working-with-feature-layers-and-features/
 
 import folium
+from folium.plugins import Geocoder, TimestampedGeoJson
 from arcgis.features import FeatureLayer
 from pyproj import Transformer
-from folium.plugins import Geocoder
-from shapely.geometry import Polygon, MultiPolygon, GeometryCollection
+from shapely.geometry import Polygon
+import datetime
 
 OTTAWA = [45.4215, -75.6972]
+RIVERSIDE = [45.2815, 75.6832]
 
 # City of Ottawa Zoning: https://ottawa.ca/en/living-ottawa/laws-licences-and-permits/laws/laws-z/zoning-law-no-2008-250/zoning-law-2008-250-consolidation#
 RESIDENTIAL_ZONES = {"R1", "R2", "R3", "R4", "R5", "RM"}
@@ -61,101 +63,83 @@ def create_timeline_map(features):
     '''creates a folium map that divides zones into layers based on consolidation year'''
 
     # create basemap
-    m = folium.Map(location=OTTAWA, zoom_start=13, name="Ottawa Zones by Consolidation Year")
-
-    # create map title
-    title_html = '''
-             <h3 align="center" style="font-size:16px"><b>{}</b></h3>
-             '''.format("City of Ottawa Zones by Consolidation Date")
+    m = folium.Map(location=RIVERSIDE, zoom_start=10)
 
     # initialize layers for each year
-    fg_2008, features_2008 = folium.FeatureGroup(name="2008"), []
-    fg_2008_2009 = folium.FeatureGroup(name="2008/2009", show=False)
-    fg_2009, features_2009 = folium.FeatureGroup(name="2009"), []
-    fg_2009_2010 = folium.FeatureGroup(name="2009/2010", show=False)
-    fg_2010, features_2010 = folium.FeatureGroup(name="2010"), []
-    fg_2010_2011 = folium.FeatureGroup(name="2010/2011", show=False)
-    fg_2011, features_2011 = folium.FeatureGroup(name="2011"), []
-    fg_2011_2012 = folium.FeatureGroup(name="2011/2012", show=False)
-    fg_2012, features_2012 = folium.FeatureGroup(name="2012"), []
-    fg_2012_2013 = folium.FeatureGroup(name="2012/2013", show=False)
-    fg_2013, features_2013 = folium.FeatureGroup(name="2013"), []
-    fg_2013_2014 = folium.FeatureGroup(name="2013/2014", show=False)
-    fg_2014, features_2014 = folium.FeatureGroup(name="2014"), []
-    fg_2014_2015 = folium.FeatureGroup(name="2014/2015", show=False)
-    fg_2015, features_2015 = folium.FeatureGroup(name="2015"), []
-    fg_2015_2016 = folium.FeatureGroup(name="2015/2016", show=False)
-    fg_2016, features_2016 = folium.FeatureGroup(name="2016"), []
-    fg_2016_2017 = folium.FeatureGroup(name="2016/2017", show=False)
-    fg_2017, features_2017 = folium.FeatureGroup(name="2017"), []
-    fg_2017_2018 = folium.FeatureGroup(name="2017/2018", show=False)
-    fg_2018, features_2018 = folium.FeatureGroup(name="2018"), []
-    fg_2018_2019 = folium.FeatureGroup(name="2018/2019", show=False)
-    fg_2019, features_2019 = folium.FeatureGroup(name="2019"), []
-    fg_2019_2020 = folium.FeatureGroup(name="2019/2020", show=False)
-    fg_2020, features_2020 = folium.FeatureGroup(name="2020"), []
-    fg_2020_2021 = folium.FeatureGroup(name="2020/2021", show=False)
-    fg_2021, features_2021 = folium.FeatureGroup(name="2021"), []
+    features_2008 = []
+    features_2008_2009 = [[]]
+    features_2009 = []
+    features_2009_2010 = [[]] 
+    features_2010 = []
+    features_2010_2011 = [[]] 
+    features_2011 = []
+    features_2011_2012 = [[]]
+    features_2012 = []
+    features_2012_2013 = [[]]
+    features_2013 = []
+    features_2013_2014 = [[]]
+    features_2014 = []
+    features_2014_2015 = [[]]
+    features_2015 = []
+    features_2015_2016 = [[]]
+    features_2016 = []
+    features_2016_2017 = [[]]
+    features_2017 = []
+    features_2017_2018 = [[]]
+    features_2018 = []
+    features_2018_2019 = [[]]
+    features_2019 = []
+    features_2019_2020 = [[]]
+    features_2020 = []
+    features_2020_2021 = [[]]
+    features_2021 = []
 
-    intermediary_layers = [fg_2008_2009, fg_2009_2010, fg_2010_2011, fg_2011_2012, fg_2012_2013, fg_2013_2014, fg_2014_2015, fg_2015_2016, fg_2016_2017, fg_2017_2018, fg_2018_2019, fg_2019_2020, fg_2020_2021]
-    yearly_layers = [features_2008, features_2009, features_2010, features_2011, features_2012, features_2013, features_2014, features_2015, features_2016, features_2017, features_2018, features_2019, features_2020, features_2021]
+    yearly_layers = [features_2008, features_2008_2009, features_2009, features_2009_2010, features_2010, features_2010_2011, features_2011, features_2011_2012, features_2012, features_2012_2013, features_2013, features_2013_2014, features_2014, features_2014_2015, features_2015, features_2015_2016, features_2016, features_2016_2017, features_2017, features_2017_2018, features_2018, features_2018_2019, features_2019, features_2019_2020, features_2020, features_2020_2021, features_2021]
 
     # organize features into layers based on consolidation date
     for feature in features:
         coordinates = convert_crs(feature.geometry['rings'][0])
         date = feature.attributes["CONS_DATE"]
+        for i in range(len(coordinates)):
+            coordinates[i][0], coordinates[i][1] = coordinates[i][1], coordinates[i][0]
         if date.endswith("2008"):
-            features_2008.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#ff0000", fill=True, fill_color="#ff0000", fill_opacity=0.5).add_to(fg_2008)
+            features_2008.append([coordinates])
         elif date.endswith("2009"):
-            features_2009.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#ff8700", fill=True, fill_color="#ff8700", fill_opacity=0.5).add_to(fg_2009)
+            features_2009.append([coordinates])
         elif date.endswith("2010"):
-            features_2010.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#ffd300", fill=True, fill_color="#ffd300", fill_opacity=0.5).add_to(fg_2010)
+            features_2010.append([coordinates])
         elif date.endswith("2011"):
-            features_2011.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#deff0a", fill=True, fill_color="#deff0a", fill_opacity=0.5).add_to(fg_2011)
+            features_2011.append([coordinates])
         elif date.endswith("2012"):
-            features_2012.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#a1ff0a", fill=True, fill_color="#a1ff0a", fill_opacity=0.5).add_to(fg_2012)
+            features_2012.append([coordinates])
         elif date.endswith("2013"):
-            features_2013.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#0aff99", fill=True, fill_color="#0aff99", fill_opacity=0.5).add_to(fg_2013)
+            features_2013.append([coordinates])
         elif date.endswith("2014"):
-            features_2014.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#0aefff", fill=True, fill_color="#0aefff", fill_opacity=0.5).add_to(fg_2014)
+            features_2014.append([coordinates])
         elif date.endswith("2015"):
-            features_2015.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#147df5", fill=True, fill_color="#147df5", fill_opacity=0.5).add_to(fg_2015)
+            features_2015.append([coordinates])
         elif date.endswith("2016"):
-            features_2016.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#580aff", fill=True, fill_color="#580aff", fill_opacity=0.5).add_to(fg_2016)
+            features_2016.append([coordinates])
         elif date.endswith("2017"):
-            features_2017.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#be0aff", fill=True, fill_color="#be0aff", fill_opacity=0.5).add_to(fg_2017)
+            features_2017.append([coordinates])
         elif date.endswith("2018"):
-            features_2018.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#7b4618", fill=True, fill_color="#7b4618", fill_opacity=0.5).add_to(fg_2018)
+            features_2018.append([coordinates])
         elif date.endswith("2019"):
-            features_2019.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#606f49", fill=True, fill_color="#606f49", fill_opacity=0.5).add_to(fg_2019)
+            features_2019.append([coordinates])
         elif date.endswith("2020"):
-            features_2020.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#b4418e", fill=True, fill_color="#b4418e", fill_opacity=0.5).add_to(fg_2020)
+            features_2020.append([coordinates])
         elif date.endswith("2021"):
-            features_2021.append(coordinates)
-            folium.Polygon(locations=coordinates, color="#ea515f", fill=True, fill_color="#ea515f", fill_opacity=0.5).add_to(fg_2021)
+            features_2021.append([coordinates])
 
     # compute intersection of layers from year-to-year (to visalize differences)
-    for i in range(len(yearly_layers)-1):
+    for i in range(len(yearly_layers)-2):
         layer_a = yearly_layers[i]
-        layer_b = yearly_layers[i + 1]
+        layer_b = yearly_layers[i + 2]
         for f1 in layer_a:
             for f2 in layer_b:
                 intersection = None
                 try:
-                    p1, p2 = Polygon(f1), Polygon(f2)
+                    p1, p2 = Polygon(f1[0]), Polygon(f2[0])
                     if p1.touches(p2) or p2.touches(p1): continue
                     if p1.intersects(p2):
                         if p1.within(p2):
@@ -170,38 +154,53 @@ def create_timeline_map(features):
                 except Exception as e:
                     print("an error occurred:", e)
                 if intersection:
-                    print("drawing intersection . . .")
+                    print("drawing intersection . . .")              
                     intersection_polygon = Polygon(intersection)
-                    folium.Polygon(locations=intersection_polygon.exterior.coords, color="#faff00", fill=True, fill_color="#faff00", fill_opacity=0.7).add_to(intermediary_layers[i])
+                    yearly_layers[i + 1].append(list(intersection_polygon.exterior.coords))
+                    # folium.Polygon(locations=intersection_polygon.exterior.coords, color="#faff00", fill=True, fill_color="#faff00", fill_opacity=0.7).add_to(yearly_layers[i + 1])
 
-    # add all layers to the basemap
-    fg_2008.add_to(m)
-    fg_2008_2009.add_to(m)
-    fg_2009.add_to(m)
-    fg_2009_2010.add_to(m)
-    fg_2010.add_to(m)
-    fg_2010_2011.add_to(m)
-    fg_2011.add_to(m)
-    fg_2011_2012.add_to(m)
-    fg_2012.add_to(m)
-    fg_2012_2013.add_to(m)
-    fg_2013.add_to(m)
-    fg_2013_2014.add_to(m)
-    fg_2014.add_to(m)
-    fg_2014_2015.add_to(m)
-    fg_2015.add_to(m)
-    fg_2015_2016.add_to(m)
-    fg_2016.add_to(m)
-    fg_2016_2017.add_to(m)
-    fg_2017.add_to(m)
-    fg_2017_2018.add_to(m)
-    fg_2018.add_to(m)
-    fg_2018_2019.add_to(m)
-    fg_2019.add_to(m)
-    fg_2019_2020.add_to(m)
-    fg_2020.add_to(m)
-    fg_2020_2021.add_to(m)
-    fg_2021.add_to(m)
+    features = []
+    curr_year = 2008
+    colors = { 2008: "#f94144", 2009: "#f3722c", 2010: "#f8961e", 2011: "#f9844a", 2012: "#f9c74f", 2013: "#90be6d", 2014: "#90be6d", 2015: "#43aa8b", 2016: "#4d908e", 2017: "#577590", 2018: "#277da1", 2019: "#54478c", 2020: "#bd4f6c", 2021: "#905a51"}
+    overlap_color = "#ffff24"
+
+    for i in range(len(yearly_layers)):
+
+        if i % 2 != 0:
+            print("overlap length: ", len(yearly_layers[i][0]))
+            color = overlap_color
+        else:
+            color = colors[curr_year]
+
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiPolygon",
+                    "coordinates": yearly_layers[i]
+                },
+                "properties": {
+                    "times": [datetime.datetime(curr_year, 1, 1).isoformat()] * len(yearly_layers[i]),
+                    "style": {
+                        "color": color,
+                        "fillColor": color,
+                        "fillOpacity": 0.5
+                    }
+                },
+            }
+        )
+
+        if i % 2 == 0:
+            curr_year += 1
+
+    TimestampedGeoJson(
+        {
+            "type": "FeatureCollection",
+            "features": features,
+        },
+        period="P1Y",
+        transition_time=1000
+    ).add_to(m)
 
     # add search to map
     Geocoder().add_to(m)
@@ -209,11 +208,8 @@ def create_timeline_map(features):
     # add legend
     folium.LayerControl().add_to(m)
 
-    # add title to map
-    m.get_root().html.add_child(folium.Element(title_html))
-
     # save map to data folder
-    m.save("data/map-timeline.html")
+    m.save("maps/map-timeTEST.html")
 
 def create_zone_map(features):
     '''creates a folium map that splits features into layers based on types of zones'''
@@ -284,7 +280,7 @@ def create_zone_map(features):
     m.get_root().html.add_child(folium.Element(title_html))
 
     # save map to data folder
-    m.save("data/map-zone.html")
+    m.save("maps/map-zone.html")
 
 def same_side(edge_start, edge_end, p):
     '''given an edge and a point determine if the point lies on the same side as the rest of the polygon'''
@@ -324,7 +320,7 @@ def main():
     print("starting .. ")
     features = get_features()
     print("retrieved " + str(len(features)) + " features")
-    # create_zone_map(features)
+    create_zone_map(features)
     create_timeline_map(features)
 
 if __name__ == "__main__":
